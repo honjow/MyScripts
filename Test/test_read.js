945e0a9b24d3b426600eaad33b4f223119aae249
honjow <hobjow311@gmail.com>
2020-02-14 04:02:14 +0800

    dix

--- Test/test_read.js
+++ Test/test_read.js
@@ -3,12 +3,12 @@ read()
 $done()
 
 function read() {
-    var value = $persistentStore.read(value, key)
-    if (!value) {
-        $notification.post("读取失败", "", "");
-        console.log("读取失败")
-      } else {
-        $notification.post("读取成功", "", "");
-        console.log("读取成功" + value)
-      }
+  var value_out = $persistentStore.read(key)
+  if (!value_out) {
+      $notification.post("读取失败", "", "");
+      console.log("读取失败")
+    } else {
+      $notification.post("读取成功", "", "");
+      console.log("读取成功" + value_out)
+    }
 }
