cb149e109fd4521598e06cfddf58d8e9d04c5e44
honjow <hobjow311@gmail.com>
2020-02-14 03:57:29 +0800

    f

--- Test/test_write.js
+++ Test/test_write.js
@@ -2,7 +2,9 @@ const key = 'testkey'
 const value = '测试内容'
 
 write()
+read()
 $done()
+
 function write() {
     var rult = $persistentStore.write(value, key)
     if (!rult) {
@@ -12,4 +14,16 @@ function write() {
         $notification.post("写入成功", "", "");
         console.log("写入成功")
       }
+}
+
+
+function read() {
+  var value = $persistentStore.read(value, key)
+  if (!value) {
+      $notification.post("读取失败", "", "");
+      console.log("读取失败")
+    } else {
+      $notification.post("读取成功", "", "");
+      console.log("读取成功" + value)
+    }
 }
