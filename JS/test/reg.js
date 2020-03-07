cb08e87faddd13390848c6f9a2129a6655bed854
honjow <honjow311@gmail.com>
2020-03-07 23:09:50 +0800

    mo

--- JS/test/reg.js
+++ JS/test/reg.js
@@ -3,7 +3,9 @@
 function reg_test() {
     var str = "abc12345d"; 
     var n = /(?<=abc)\d+(?=d)/.exec(str);
-    console.log(n[0])
+    msg = '输入字符:' + n[0] + '\n匹配结果 ：' + n
+    console.log(msg)
+    $notification.post('', '', msg)
 }
 
 reg_test()
