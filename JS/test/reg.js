3a62d24d766b306347674c26f33de28161f9c280
honjow <honjow311@gmail.com>
2020-03-07 23:27:10 +0800

    t

--- JS/test/reg.js
+++ JS/test/reg.js
@@ -2,10 +2,10 @@ reg_test()
 
 function reg_test() {
     var str = "abc12345d"; 
-    var patt = /(?<=abc)\d+(?=d)/;
+    var patt = new RegExp('?<=abc)\d+(?=d)');
     var n = patt.exec(str);
     msg = '输入字符:' + n[0] + '\n匹配结果 ：' + n
     console.log(msg)
-    $notification.post('', '', msg)
+    // $notification.post('', '', msg)
 }
 
